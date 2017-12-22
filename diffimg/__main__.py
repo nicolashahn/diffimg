ea0418a5b34fa90798f589e32b7e8c93ba63b57c
Nicolas Hahn <nicolas@stonespring.org>
2017-12-21 17:08:17 -0800

    __init__.py and __main__.py better organized

--- diffimg/__main__.py
+++ diffimg/__main__.py
@@ -1,3 +1,44 @@
-from __init__ import main
+from __init__ import diff, diff_img_file
+import argparse
+import sys
 
-main()
+def get_args():
+    parser = argparse.ArgumentParser(
+            description='Generate a diff image from two images \
+            and/or find difference percentage')
+    parser.add_argument('image1', 
+            type=str, 
+            help='first image')
+    parser.add_argument('image2', 
+            type=str, 
+            help='second image')
+    parser.add_argument('--ratio', '-r',
+            dest='use_ratio', 
+            action='store_true', 
+            help='return a ratio instead of percentage')
+    parser.add_argument('--delete', '-d',
+            dest='delete_diff_file',
+            action='store_true', 
+            help='delete diff image file')
+    parser.add_argument('--filename', '-f',
+            dest='diff_img_file', 
+            type=str,
+            default=diff_img_file,
+            help='filename with valid extension to store diff image \
+                    (defaults to diff_img.jpg)')
+    return parser.parse_args()
+
+def main():
+    args = get_args()
+    diff_ratio = diff(args.image1, 
+                      args.image2, 
+                      args.delete_diff_file, 
+                      args.diff_img_file)
+    if args.use_ratio:
+        print(diff_ratio)
+    else:
+        print('Images differ by {}%'.format(diff_ratio*100))
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ except ImportError:
 setup(
     name = 'diffimg',
     packages = ['diffimg'], # this must be the same as the name above
-    version = '0.1.4',
+    version = '0.1.5',
     description = 'Get the % difference in images + generate a diff image',
     author = 'Nicolas Hahn',
     author_email = 'nicolas@stonespring.org',
     url = 'https://github.com/nicolashahn/python-image-diff',
-    download_url = 'https://github.com/nicolashahn/python-image-diff/archive/v0.1.4.tar.gz',
+    download_url = 'https://github.com/nicolashahn/python-image-diff/archive/v0.1.5.tar.gz',
     keywords = ['diff', 'difference', 'image'],
     classifiers = [],
     install_requires = ['Pillow>=4.3'],
