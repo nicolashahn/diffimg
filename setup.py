23d21303932c63fb0880475979c8c3a0d8651d07
Nicolas Hahn <nicolas@stonespring.org>
2017-12-21 16:43:16 -0800

    update setup.py, add PIL requirement

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 setup(
-  name = 'diffimg',
-  packages = ['diffimg'], # this must be the same as the name above
-  version = '0.1',
-  description = 'Get the % difference in images + generate a diff image Edit',
-  author = 'Nicolas Hahn',
-  author_email = 'nicolas@stonespring.org',
-  url = 'https://github.com/nicolashahn/python-image-diff',
-  download_url = 'https://github.com/nicolashahn/python-image-diff/archive/0.1.tar.gz',
-  keywords = ['diff', 'difference', 'image'],
-  classifiers = [],
+    name = 'diffimg',
+    packages = ['diffimg'], # this must be the same as the name above
+    version = '0.1.1',
+    description = 'Get the % difference in images + generate a diff image Edit',
+    author = 'Nicolas Hahn',
+    author_email = 'nicolas@stonespring.org',
+    url = 'https://github.com/nicolashahn/python-image-diff',
+    download_url = 'https://github.com/nicolashahn/python-image-diff/archive/0.1.1.tar.gz',
+    keywords = ['diff', 'difference', 'image'],
+    classifiers = [],
+    install_requires = ['Pillow>=4.3'],
 )
