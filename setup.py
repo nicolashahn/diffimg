1d0dd3e6aa935cc212d2a9b282822e781b593f00
Nicolas Hahn <nicolas@stonespring.org>
2017-12-21 16:45:52 -0800

    change tag to 0.1.3

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ except ImportError:
 setup(
     name = 'diffimg',
     packages = ['diffimg'], # this must be the same as the name above
-    version = '0.1.1',
+    version = '0.1.3',
     description = 'Get the % difference in images + generate a diff image Edit',
     author = 'Nicolas Hahn',
     author_email = 'nicolas@stonespring.org',
     url = 'https://github.com/nicolashahn/python-image-diff',
-    download_url = 'https://github.com/nicolashahn/python-image-diff/archive/0.1.1.tar.gz',
+    download_url = 'https://github.com/nicolashahn/python-image-diff/archive/0.1.3.tar.gz',
     keywords = ['diff', 'difference', 'image'],
     classifiers = [],
     install_requires = ['Pillow>=4.3'],
