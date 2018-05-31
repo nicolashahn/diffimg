9493dbbeb741243e4be12a70dbccd3f7e4852734
Nicolas Hahn <nicolas@stonespring.org>
2018-05-31 10:38:25 -0400

    refactor, more tests

--- diffimg/__init__.py
+++ diffimg/__init__.py
@@ -4,29 +4,33 @@
 # only works with images of the same file type
 
 from PIL import Image, ImageChops, ImageStat
-from os import remove
 
-diff_img_file = 'diff_img.jpg'
+DIFF_IMG_FILE = 'diff_img.jpg'
 
 def diff(im1_file, 
          im2_file, 
          delete_diff_file=False, 
-         diff_img_file=diff_img_file):
+         diff_img_file=DIFF_IMG_FILE):
     '''Calculate the difference between two images of the same size
     by comparing channel values at the pixel level. 
 
     `delete_diff_file`: removes the diff image after ratio found
     `diff_img_file`: filename to store diff image
     '''
+
+    # Generate diff image in memory.
     im1 = Image.open(im1_file)
     im2 = Image.open(im2_file)
     diff_img = ImageChops.difference(im1,im2)
-    diff_img.convert('RGB').save(diff_img_file)
+
+    if not delete_diff_file:
+        diff_img.convert('RGB').save(diff_img_file)
+
+    # Calculate difference as a ratio.
     stat = ImageStat.Stat(diff_img)
-    # can be [r,g,b] or [r,g,b,a]
+    # Can be [r,g,b] or [r,g,b,a].
     sum_channel_values = sum(stat.mean)
     max_all_channels = len(stat.mean) * 100
     diff_ratio = sum_channel_values/max_all_channels
-    if delete_diff_file:
-        remove(diff_img_file)
+
     return diff_ratio
--- test.py
+++ test.py
@@ -1,24 +1,40 @@
+import os
 import unittest
 from diffimg import diff
-import os
 
-class TestDiffImg(unittest.TestCase):
 
-    def test_mario_images(self):
+IMAGES_DIR = 'images'
+IMG_PATH = lambda f: os.path.join(IMAGES_DIR, f)
 
-        images_dir = 'images'
-        img_path = lambda f: os.path.join(images_dir, f)
+IMG1 = IMG_PATH('mario-circle-cs.png')
+IMG2 = IMG_PATH('mario-circle-node.png')
+TEST_FILE = IMG_PATH('test-diff.jpg')
 
-        img1 = img_path('mario-circle-cs.png')
-        img2 = img_path('mario-circle-node.png')
-        diff_img_file = img_path('test-diff.jpg')
 
-        ratio = diff(
-                img1, img2,
-                delete_diff_file=True,
-                diff_img_file=diff_img_file)
+class TestAll(unittest.TestCase):
 
+    def test_ratio(self):
+        ratio = diff(IMG1,
+                     IMG2,
+                     delete_diff_file=True)
         self.assertEqual(ratio, 0.01866502624671916)
+
+    def test_delete_diff_img(self):
+        if os.path.exists(TEST_FILE):
+            os.remove(TEST_FILE)
+        diff(IMG1,
+             IMG2,
+             delete_diff_file=True,
+             diff_img_file=TEST_FILE)
+        self.assertFalse(os.path.exists(TEST_FILE))
+
+    def test_make_diff_img(self):
+        diff(IMG1,
+             IMG2,
+             diff_img_file=TEST_FILE)
+        self.assertTrue(os.path.exists(TEST_FILE))
+        os.remove(TEST_FILE)
+
 
 if __name__ == '__main__':
     unittest.main()
