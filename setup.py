0b1e0cc5c820399af46af6535609b11bd2682972
Nicolas Hahn <nicolas@stonespring.org>
2018-07-25 20:00:52 -0400

    update tag

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
-version = '0.1.8'
+VERSION = '0.1.9'
 setup(
-    name = 'diffimg',
-    packages = ['diffimg'], # this must be the same as the name above
-    version = version,
-    description = 'Get the % difference in images + generate a diff image',
-    author = 'Nicolas Hahn',
-    author_email = 'nicolas@stonespring.org',
-    url = 'https://github.com/nicolashahn/python-image-diff',
-    download_url = 'https://github.com/nicolashahn/python-image-diff/archive/v{}.tar.gz'.format(version),
-    keywords = ['diff', 'difference', 'image'],
-    classifiers = [],
-    install_requires = ['Pillow>=4.3'],
+    name='diffimg',
+    packages=['diffimg'],  # this must be the same as the name above
+    version=VERSION,
+    description='Get the % difference in images + generate a diff image',
+    author='Nicolas Hahn',
+    author_email='nicolas@stonespring.org',
+    url='https://github.com/nicolashahn/python-image-diff',
+    download_url='https://github.com/nicolashahn/python-image-diff/archive/v{}.tar.gz'.format(VERSION),
+    keywords=['diff', 'difference', 'image'],
+    classifiers=[],
+    install_requires=['Pillow>=4.3'],
 )
